3a15f3cae31a0c36327eea902b8ba51472fda6ba
Myracle <max.weber.consultant@outlook.com>
2018-06-17 20:16:44 +0200

    feature: Add a interactive text input field

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,36 @@ class FriendlychatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
+
+  Widget _buildTextComposer() {
+    return new Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: new TextField(
+        controller: _textController,
+        onSubmitted: _handleSubmitted,
+        decoration: new InputDecoration.collapsed(hintText: "Send a message"),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Friendlychat"),
       ),
+      body: _buildTextComposer(),
     );
-
   }
 
+  void _handleSubmitted(String value) {
+    _textController.clear();
+  }
 }
