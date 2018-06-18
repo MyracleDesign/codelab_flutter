81823f64a5f301b6f47ad41da5e1d5145cbd4365
Myracle <max.weber.consultant@outlook.com>
2018-06-18 07:45:12 +0200

    feature: Added a basic chat message

--- lib/main.dart
+++ lib/main.dart
@@ -23,14 +23,28 @@ class ChatScreenState extends State<ChatScreen> {
   final TextEditingController _textController = new TextEditingController();
 
   Widget _buildTextComposer() {
-    return new Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: new TextField(
-        controller: _textController,
-        onSubmitted: _handleSubmitted,
-        decoration: new InputDecoration.collapsed(hintText: "Send a message"),
-      ),
-    );
+    return new IconTheme(
+        data: new IconThemeData(color: Theme.of(context).accentColor),
+        child: new Container(
+            margin: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: new Row(
+              children: <Widget>[
+                new Flexible(
+                  child: new TextField(
+                    controller: _textController,
+                    onSubmitted: _handleSubmitted,
+                    decoration: new InputDecoration.collapsed(
+                        hintText: "Send a message"),
+                  ),
+                ),
+                new Container(
+                    margin: new EdgeInsets.symmetric(horizontal: 4.0),
+                    child: new IconButton(
+                        icon: new Icon(Icons.send),
+                        onPressed: () =>
+                            _handleSubmitted(_textController.text)))
+              ],
+            )));
   }
 
   @override
@@ -45,5 +59,40 @@ class ChatScreenState extends State<ChatScreen> {
 
   void _handleSubmitted(String value) {
     _textController.clear();
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+
+  static const String _name = "Max Weber";
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: new Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: new CircleAvatar(child: new Text(_name[0])),
+            ),
+            new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Text(
+                  _name,
+                  style: Theme.of(context).textTheme.subhead,
+                ),
+                new Container(
+                    margin: const EdgeInsets.only(top: 5.0),
+                    child: new Text(text))
+              ],
+            )
+          ],
+        ));
   }
 }
