919db131453d1ac48f6ceec6dc635535453699ea
Myracle <max.weber.consultant@outlook.com>
2018-06-18 22:57:54 +0200

    feature: ChatMessages are now visible for the users

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,25 @@ class ChatScreenState extends State<ChatScreen> {
       appBar: new AppBar(
         title: new Text("Friendlychat"),
       ),
-      body: _buildTextComposer(),
+      body: new Column(
+        children: <Widget>[
+          new Flexible(
+            child: new ListView.builder(
+              padding: new EdgeInsets.all(8.0),
+              itemBuilder: (_, int index) => _messages[index],
+              reverse: true,
+              itemCount: _messages.length,
+            ),
+          ),
+          new Divider(
+            height: 1.6,
+          ),
+          new Container(
+            decoration: new BoxDecoration(color: Theme.of(context).cardColor),
+            child: _buildTextComposer(),
+          )
+        ],
+      ),
     );
   }
 
